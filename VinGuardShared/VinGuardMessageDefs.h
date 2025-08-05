e048253bb6db616b8372c7da6c05db310117d1e5
Vinith Raj <vintihbraj@gmail.com>
2025-08-04 18:14:42 -0700

    +Add copyright header

--- VinGuardShared/VinGuardMessageDefs.h
+++ VinGuardShared/VinGuardMessageDefs.h
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2025 Vinith Raj. All rights reserved.
+ *
+ * Permission is hereby granted to view and read this source code
+ * for educational or informational purposes only.
+ *
+ * Redistribution, modification, reproduction, or use in any software—whether
+ * commercial, open-source, or personal—is strictly prohibited without prior
+ * written permission from the author.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT.
+ *
+ * Contact: vinithbraj(at)gmail(dot)com
+ */
+
 #pragma once
 
 #include "VinGuardKSerializer.h"
